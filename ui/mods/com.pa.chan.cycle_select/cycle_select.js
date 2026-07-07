547b1b8fa7c3f02bac4a14ee52b560a5ca5b1872
Chan <thedigitalchan@gmail.com>
2026-07-07 22:02:55 +0200

    [Project, Mod] Added keybinding & ability to cycle backwards through the current selection. #

--- ui/mods/com.pa.chan.cycle_select/cycle_select.js
+++ ui/mods/com.pa.chan.cycle_select/cycle_select.js
@@ -1,26 +1,51 @@
 (function() 
 {
-    var hotkey_id = 'cycle_through_current_selection';
     var hotkey_set_name = 'gameplay';
     var hotkey_display_sub_group_name = 'Cycle Select';
 
-    action_sets[hotkey_set_name][hotkey_id] = function () 
     {
-        if (model[hotkey_id]) 
-        {
-            model[hotkey_id].apply(this, arguments);
-        }
-    };
+        var hotkey_id_cycle_select_forward = 'cycle_forward_through_current_selection';
 
-    api.settings.definitions.keyboard.settings.cycle_through_current_selection = 
+        action_sets[hotkey_set_name][hotkey_id_cycle_select_forward] = function () 
+        {
+            if (model[hotkey_id_cycle_select_forward]) 
+            {
+                model[hotkey_id_cycle_select_forward].apply(this, arguments);
+            }
+        };
+
+        api.settings.definitions.keyboard.settings.cycle_forward_through_current_selection = 
+        {
+            title: 'Cycle Forward Through Current Selection',
+            type: 'keybind',
+            set: hotkey_set_name,
+            display_group: 'mods',
+            display_sub_group: hotkey_display_sub_group_name,
+            default: ''
+        };
+    }
+
     {
-        title: 'Cycle Through Current Selection',
-        type: 'keybind',
-        set: hotkey_set_name,
-        display_group: 'mods',
-        display_sub_group: hotkey_display_sub_group_name,
-        default: ''
-    };
+        var hotkey_id_cycle_select_backwards = 'cycle_backwards_through_current_selection';
+
+        action_sets[hotkey_set_name][hotkey_id_cycle_select_backwards] = function () 
+        {
+            if (model[hotkey_id_cycle_select_backwards]) 
+            {
+                model[hotkey_id_cycle_select_backwards].apply(this, arguments);
+            }
+        };
+
+        api.settings.definitions.keyboard.settings.cycle_backwards_through_current_selection = 
+        {
+            title: 'Cycle Backwards Through Current Selection',
+            type: 'keybind',
+            set: hotkey_set_name,
+            display_group: 'mods',
+            display_sub_group: hotkey_display_sub_group_name,
+            default: ''
+        };
+    }
 
     api.Panel.message('', 'inputmap.reload');
 
--- ui/mods/com.pa.chan.cycle_select/live_game.js
+++ ui/mods/com.pa.chan.cycle_select/live_game.js
@@ -2,56 +2,94 @@
 {
     var current_selection_unit_type_keys = [];
     var current_selection_unit_types = [];
-    var current_cycle_index = 0;
+    const CYCLE_INDEX_DEFAULT_VALUE = -1;
+    var current_cycle_index = CYCLE_INDEX_DEFAULT_VALUE;
     var should_prevent_selection_state_reset = false;
 
     var reset_selection_state = function()
     {
         current_selection_unit_type_keys = [];
         current_selection_unit_types = [];
-        current_cycle_index = 0;
+        current_cycle_index = CYCLE_INDEX_DEFAULT_VALUE;
     };
+
+    function math_modulo(a_n, a_m) 
+    {
+        return ((a_n % a_m) + a_m) % a_m;
+    }
 
     var increment_cycle_index = function()
     {
-        current_cycle_index = (current_cycle_index + 1) % current_selection_unit_type_keys.length
+        current_cycle_index = math_modulo((current_cycle_index + 1), current_selection_unit_type_keys.length);
     }
 
-    model.cycle_through_current_selection = function() 
-    {  
+    var decrement_cycle_index = function()
+    {
+        current_cycle_index = math_modulo((current_cycle_index - 1), current_selection_unit_type_keys.length);
+    }
+
+    var game_select_types_in_current_selection = function(a_index)
+    {
+        const unit_type_key = current_selection_unit_type_keys[a_index];
+        const units_result = current_selection_unit_types[unit_type_key];
+        
+        engine.call('select.byIds', units_result);
+
+        return true;
+    }
+
+    model.cycle_forward_through_current_selection = function() 
+    {
         if (current_selection_unit_type_keys.length <= 1)
         {
-            // console.log("CycleSelect; INFO - Single Unit type select, no need to cycle between unit types.");
+            // console.log("CycleSelect; INFO - Single Unit type select, no need to cycle (forward) between unit types.");
             return;
         }
 
-        var unit_type_key = current_selection_unit_type_keys[current_cycle_index];
-        var units_result = current_selection_unit_types[unit_type_key];
-        
-        engine.call('select.byIds', units_result);
-        
         increment_cycle_index();
-
+        
+        game_select_types_in_current_selection(current_cycle_index);
+        
         should_prevent_selection_state_reset = true;
-
     };
 
-    model.selection_subscriber = function(payload) 
+    model.cycle_backwards_through_current_selection = function() 
     {
-        if (payload && model.mode() != 'select') 
+        if (current_selection_unit_type_keys.length <= 1)
+        {
+            // console.log("CycleSelect; INFO - Single Unit type select, no need to cycle (backwards) between unit types.");
+            return;
+        }
+
+        const is_first_cycle_after_selection = (current_cycle_index == CYCLE_INDEX_DEFAULT_VALUE);
+        if (is_first_cycle_after_selection)
+        {
+            current_cycle_index = 0;
+        }
+        
+        decrement_cycle_index();
+        
+        game_select_types_in_current_selection(current_cycle_index);
+        
+        should_prevent_selection_state_reset = true;
+    };
+
+    model.selection_subscriber = function(a_payload) 
+    {
+        if (a_payload && model.mode() != 'select') 
         {
             if (!should_prevent_selection_state_reset)
             {
                 reset_selection_state();
                 
-                if (!payload.spec_ids)
+                if (!a_payload.spec_ids)
                 {
                     console.log("CycleSelect; ERROR - An attempt to select units, but no types received.")
                     return;
                 }
                 
-                current_selection_unit_type_keys = Object.keys(payload.spec_ids);;
-                current_selection_unit_types = payload.spec_ids;
+                current_selection_unit_type_keys = Object.keys(a_payload.spec_ids);;
+                current_selection_unit_types = a_payload.spec_ids;
                 
             }
 
